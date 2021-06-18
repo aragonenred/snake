1bec23038228420e2bebd6f16df05a14796a6210
Edu Note <aragonenred@gmail.com>
2021-06-18 13:59:49 -0300

    Correcciones

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@ document.addEventListener('DOMContentLoaded', function(){
     const imgGameOver = document.querySelector('#imgGameOver');
     const score = document.querySelector('#score');
     const canvas = document.querySelector('#canvas');
+    const btn_up = document.querySelector('#up');
+    const btn_down = document.querySelector('#down');
+    const btn_left = document.querySelector('#left');
+    const btn_right = document.querySelector('#right');
     
     
     backgroundColor = '#959e80';
@@ -10,50 +14,52 @@ document.addEventListener('DOMContentLoaded', function(){
     food = new Food();
     game = new Game();
 
+
+    btn_up.addEventListener('click', function(e){
+        console.log('up');
+        e.preventDefault();
+        up();
+
+    });
+    btn_down.addEventListener('click', function(e){
+        console.log('down');
+        e.preventDefault();
+        down();
+    });
+    btn_left.addEventListener('click', function(e){
+        console.log('left');
+        e.preventDefault();
+        left();
+    });
+    btn_right.addEventListener('click', function(e){
+        console.log('right');
+        e.preventDefault();
+        right();
+    });
+
+
+
     game.start();
+   
    
   
     window.addEventListener('keydown', function(e) {
         switch (e.key) {
         case 'ArrowUp':   
             e.preventDefault();
-            /**Valido que el movimiento anterior se haya dibujado */
-            if(snake.moved == true){
-                snake.vx = 0;
-                /**Si no estoy yendo en esa dirección.. */
-                if(snake.vy == 0){
-                    snake.vy = snake.step * (-1);
-                    snake.moved = false;
-                }
-            }
+            up();
             break;
         case 'ArrowRight':
-            if(snake.moved == true){     
-                snake.vy = 0;
-                if(snake.vx == 0){ 
-                    snake.vx = snake.step;
-                    snake.moved = false;
-                }
-            }
+            e.preventDefault();
+            right();
             break;
         case 'ArrowDown':  
             e.preventDefault();  
-            if(snake.moved == true){
-                snake.vx = 0;
-                if(snake.vy == 0){
-                    snake.vy = snake.step;
-                    snake.moved = false;
-                }
-            }
+            down();
             break;
         case 'ArrowLeft':
-            if(snake.moved == true){
-                snake.vy = 0;
-                if(snake.vx == 0){
-                    snake.vx = snake.step * (-1);
-                    snake.moved = false;
-                }
-            }
+            e.preventDefault();
+            left();
             break;
         
         case 'p':
@@ -67,13 +73,48 @@ document.addEventListener('DOMContentLoaded', function(){
                 game.status = 'run';
                 snake.move(true);
             }
-            break;
-        case 'j':
-  
-            
-        
+            break; 
         }  
     });
+
+    function up(){
+        /**Valido que el movimiento anterior se haya dibujado */
+        if(snake.moved == true){
+            snake.vx = 0;
+            /**Si no estoy yendo en esa dirección.. */
+            if(snake.vy == 0){
+                snake.vy = snake.step * (-1);
+                snake.moved = false;
+            }
+        }
+    }
+    function down(){
+        if(snake.moved == true){
+            snake.vx = 0;
+            if(snake.vy == 0){
+                snake.vy = snake.step;
+                snake.moved = false;
+            }
+        }
+    }
+    function left(){
+        if(snake.moved == true){
+            snake.vy = 0;
+            if(snake.vx == 0){
+                snake.vx = snake.step * (-1);
+                snake.moved = false;
+            }
+        }
+    }
+    function right(){
+        if(snake.moved == true){     
+            snake.vy = 0;
+            if(snake.vx == 0){ 
+                snake.vx = snake.step;
+                snake.moved = false;
+            }
+        }
+    }
 
 
 
